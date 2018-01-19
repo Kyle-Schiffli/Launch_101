3c6d4bc1225c9ed3301f3a3405897cf8b33c16e6
Kyle Schiffli <kyle.schiffli@gmail.com>
2018-01-19 02:15:03 +0000

    added odd file

---
+++ 101_109_small_problems/easy_1/odd.rb
@@ -0,0 +1,11 @@
+#Write a method that takes one integer argument, 
+#which may be positive, negative, or zero. 
+#This method return true if the number's absolute value is odd. 
+#You may assume that the argument is a valid integer value.
+
+def is_odd(integer)
+  integer.odd?
+end
+
+puts is_odd(5)
+puts is_odd(6)
