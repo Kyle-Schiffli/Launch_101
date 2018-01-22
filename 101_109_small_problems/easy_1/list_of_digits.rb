9e6dff4083901bd67ce3b4f2746887da4fcd252c
Kyle Schiffli <kyle.schiffli@gmail.com>
2018-01-22 14:22:24 +0000

    list of digits

---
+++ 101_109_small_problems/easy_1/list_of_digits.rb
@@ -0,0 +1,16 @@
+# Write a method that takes one argument, a positive integer,
+# and returns a list of the digits in the number.
+
+#puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+#puts digit_list(7) == [7]                     # => true
+
+
+def digit_list(int)
+arr = int.to_s.split('')
+arr2 = []
+arr.each {|x| arr2.push(x.to_i)}
+return arr2
+end
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]    
+puts digit_list(7) == [7]    
--- 101_109_small_problems/easy_1/odd.rb
+++ 101_109_small_problems/easy_1/odd.rb
@@ -1,11 +1,11 @@
-#Write a method that takes one integer argument, 
-#which may be positive, negative, or zero. 
-#This method return true if the number's absolute value is odd. 
-#You may assume that the argument is a valid integer value.
+# Write a method that takes one integer argument,
+# which may be positive, negative, or zero.
+# This method return true if the number's absolute value is odd.
+# You may assume that the argument is a valid integer value
 
-def is_odd(integer)
-  integer.odd?
+def is_odd?(integer)
+  integer % 2 == 1
 end
 
-puts is_odd(5)
-puts is_odd(6)
+puts is_odd?(5)
+puts is_odd?(6)
